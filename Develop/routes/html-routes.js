9f9a3e703ec4b8bf814aa3d6a8067af80e5f2bb1
Megan Glorioso <meganglorioso2016@gmail.com>
2023-07-02 22:07:30 -0700

    troubleshooting paths

--- Develop/routes/html-routes.js
+++ Develop/routes/html-routes.js
@@ -1,22 +1,15 @@
 const path = require('path');
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
 
+// Route to serve the notes.html file
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
 
-  // Route to serve the index.html file
-  //router.get('/', (req, res) => {
-    //res.sendFile(path.join(__dirname, '../public/index.html'));
-  //});
-
-  // Route to serve the notes.html file
-  router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
-  });
-
-  // Default route for handling 404 errors
-  router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
-
+// Default route for handling 404 errors
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 module.exports = router;
